d8f24b39d43f1ab3b63f06e81eb3bac23f6357d4
erinbedri <erinbedri@gmail.com>
2022-10-20 16:23:07 +0200

    register

--- project/users/urls.py
+++ project/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from project.users import views
+from project.users.views import RegisterView
 
 urlpatterns = [
     path('', views.home, name='users-home'),
+    path('register/', RegisterView.as_view(), name='users-register'),
 ]
--- project/users/views.py
+++ project/users/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.views import View
+
+from project.users.forms import RegisterForm
 
 
 def home(request):
     return render(request, 'users/home.html')
+
+
+class RegisterView(View):
+    form_class = RegisterForm
+    template_name = 'users/register.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+
+            return redirect('users-home')
+
+        return render(request, self.template_name, {'form': form})
